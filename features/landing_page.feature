00d807a79bcb1508704d3499d09398a17b37f4b2
Siianko <tsiyanko@gmail.com>
2018-07-04 11:09:29 +0200

    login, sign up, CA Mailboxer links went green

--- features/landing_page.feature
+++ features/landing_page.feature
@@ -5,8 +5,8 @@ Feature: user visits landing page
 
     Scenario: landing page view
         When I visit the landing page
-        Then I should see "Login" link
-        And I should see sign up link
-        And I should see home link
-        And I should see welcome message
+        Then I should see "Login" link 
+        And I should see "Sign up" link
+        And I should see "CA Mailboxer" link
+        And I should see "Craft Academy Mailboxer" message
     
--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -2,15 +2,8 @@ When("I visit the landing page") do
     visit root_path
 end
 
-Then("I should see {string} link") do |login|
-    expect(page).to have_link login
+Then("I should see {string} link") do |link_name|
+    expect(page).to have_link link_name
 end
 
-Then("I should see sign up link") do
-    #binding.pry 
-    pending # Write code here that turns the phrase above into concrete actions
-end
 
-Then("I should see home link") do
-    pending # Write code here that turns the phrase above into concrete actions
-end
