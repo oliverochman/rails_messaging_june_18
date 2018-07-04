9659662e5eabb1f0e861536e729613cdcd650ee5
Siianko <tsiyanko@gmail.com>
2018-07-04 10:48:00 +0200

    login test went green

--- features/landing_page.feature
+++ features/landing_page.feature
@@ -1,12 +1,12 @@
 Feature: user visits landing page
     As a user,
     When I visit landing page
-    I would like to see a landing page, with login and sign up, home and login buttons and a welcome message
+    I would like to see a landing page, with login and sign up, home and login links and a welcome message
 
     Scenario: landing page view
         When I visit the landing page
-        Then I should see login button
-        And I should see sign up button
-        And I should see home button
+        Then I should see "Login" link
+        And I should see sign up link
+        And I should see home link
         And I should see welcome message
     
--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -1,20 +1,16 @@
 When("I visit the landing page") do
     visit root_path
-  end
-  
-  Then("I should see login button") do
+end
+
+Then("I should see {string} link") do |login|
+    expect(page).to have_link login
+end
+
+Then("I should see sign up link") do
+    #binding.pry 
     pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  And("I should see sign up button") do
+end
+
+Then("I should see home link") do
     pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  And("I should see home button") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  And("I should see welcome message") do
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
+end
